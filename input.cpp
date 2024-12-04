1bf3bcda03fddc6992a7c3ae53f438c1106f6d7c
moner124 <74378694+moner124@users.noreply.github.com>
2024-12-04 19:37:09 +0300

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -18,20 +18,34 @@ int main(){
 //we can fix that just we have to add funcation std::getline(std::cin , name);
 //lets tryit
 
+// std::string name;
+// int age;
+
+
+// std::cout << "what is your name full name  : " ;
+// std:getline(std::cin , name);
+// std::cout << "Hello " << name << "\n";
+
+// std::cout << "what is you age : ";
+// std::cin >> age ;
+// std::cout << "your are " << age << " year old ";
+
+//if revers the name in put the age up will have problem disblint so we can fix it like that 
+//std::getline(std::cin >> std::ws , name );    like that down
+
 std::string name;
 int age;
 
 
-std::cout << "what is your name full name  : " ;
-std:getline(std::cin , name);
-std::cout << "Hello " << name << "\n";
+
 
 std::cout << "what is you age : ";
 std::cin >> age ;
 std::cout << "your are " << age << " year old ";
 
-
-
+std::cout << "what is your name full name  : " ;
+std:getline(std::cin >> std::ws , name);
+std::cout << "Hello " << name << "\n";
 
     return 0;        
 }
